391a4a4e19161a5d9f1abc4a36b7f23685efc30b
juju9595 <eunju220926@gmail.com>
2025-07-02 14:21:34 +0900

    자바 1일차 오후 2시

--- src/day04/Example1.java
+++ src/day04/Example1.java
@@ -1,5 +1,7 @@
 package day04;
 
+import java.util.Scanner;
+
 public class Example1 {//class s
     public static void main(String[] args) { //main s
 
@@ -26,8 +28,55 @@ public class Example1 {//class s
                 5. 주요 키워드 :
                     1) break;       : 가장 가까운 반복문 {} 종료/탈출       vs return
                     2) continue;    : 가장 가까운 반복문(증감식) 이동
+                6. 무한루프
+                    방법1) for( ;;){}
+                    방법2) while(true){}
 
          */
+
+        // [1] 1부터 5까지 출력
+            // 반복문 : 1부터 5까지 1씩증가 , 인덱스 0부터 마지막인덱스까지 인덱스 1씩 증가
+        for( int i = 1; i<=5 ; i++){
+            System.out.println(i);
+        }
+
+        // [2] 중첩 : 구구단
+            // 단 반복 : 2부터 9까지 1씩 증가.
+        for(int 단 = 2; 단<=9; 단++){
+            System.out.println(단);
+        }
+            // 곱 반복 : 1부터 9까지 1씩증가.
+        for(int 곱 = 2; 곱<=9; 곱++){
+            System.out.println(곱);
+        }
+            // 합치기 : (1) 단 마다 곱, (2) 곱 마다 단
+        for(int 단 = 2; 단<=9; 단++){
+            for(int 곱 = 2; 곱<=9; 곱++){
+            System.out.printf("%d X %d = %d \n", 단, 곱, 단*곱);
+            }//for2 end
+        }// for1 end
+
+        // [3] break : 1부터 10까지 출력, 만약에 5를 찾았으며 반복문 종료
+        for(int index = 1 ; index<=10 ; index++){
+            if (index == 5){break;}//만약에 현재 index가 5이면 가장 가까운 반복문{} 종료
+            System.out.println(index);
+
+        }//for end
+
+        // [4] continue : 1부터 10까지 반복, 만약에 5를 찾았으면 출력 생략
+        for(int index =1; index <=10; index ++){
+            if(index == 5){continue;} // 만약에 현재 index가 5이면 가장 가까운 반복문(증감식)으로 이동
+            System.out.println(index);
+        }
+
+        // [5] for( ; ; ){} : 무한반복 , 종료조건은 필요
+        for(; ;){
+        //while(true){
+            System.out.println("무한입력 [종료:'x'] :");
+            String input = new Scanner(System.in).next(); //키볻로부터 입력받아 저장
+            if(input.equals("x")){break;} // 만약에 입력한 문자열이 "x" 이면 반복문 탈출, 무한루프 종료
+        }//
+
 
     }//main e
 }//class e
