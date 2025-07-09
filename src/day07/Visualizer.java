0b713d2bf7f1d06b5b292f51df25730f65867b09
juju9595 <eunju220926@gmail.com>
2025-07-09 10:12:03 +0900

    오전 10시

--- src/day07/Visualizer.java
+++ src/day07/Visualizer.java
@@ -5,8 +5,8 @@ public class Visualizer {//class
     String getStar(int 정수) {
         String star = "";
         for (int i = 1; i <= 정수; i++) {
-            star += "★";
+            star += "★"; // 한번씩 1부터 x까지
         }//for e
-        return star;
-    }
+        return star; // 최종적인 문자열 반환
+    }//func e
 }//class e
